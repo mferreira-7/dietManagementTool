194942f3ba2c4bdf7c2b85eb2b9683c3eb9b7829
segun akorede <sa21282@essex.ac.uk>
2023-10-30 19:40:33 +0000

    segun

--- DietManagementTool/src/HelloWorld.java
+++ DietManagementTool/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
         System.out.println("Rafay was here...");
         System.out.println("Vladyslav was here...");
         System.out.println("Marcel was here...");
-        System.out.println("Segun was here");
+        System.out.println("Segun was here...");
         System.out.println("Rachapol was here...");
     }
 }
