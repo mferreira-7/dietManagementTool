d0a996030ccb65756632674832508dff32e9d2dc
Ege Ozen <eo22434@essex.ac.uk>
2023-10-31 15:03:02 +0000

    Changes have been done

--- DietManagementTool/src/HelloWorld.java
+++ DietManagementTool/src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Marcel was here...");
         System.out.println("Segun was here...");
         System.out.println("Rachapol was here...");
+        System.out.println("Ege was here...");
     }
 }
