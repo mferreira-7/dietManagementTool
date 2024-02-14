d85b9bc897c7e9c4cbccadd6d2516a4b0b8f9b96
Segun Akorede <sa21282@essex.ac.uk>
2024-02-14 09:52:57 +0000

    delete the dataset folder and rearange the images

--- DietManagementTool/src/main/app/view/LoginFrame.java
+++ DietManagementTool/src/main/app/view/LoginFrame.java
@@ -96,7 +96,8 @@ public class LoginFrame extends JFrame {
         // Add a logo to the top panel
         JLabel logoLabel = new JLabel(); // Create a label to hold the logo
         logoLabel.setHorizontalAlignment(JLabel.CENTER); // Set the logo to align center
-        ImageIcon logoIcon = new ImageIcon("/Users/segzzy4real/Downloads/logo.png"); // Replace with your logo path
+        URL logoUrl = getClass().getResource("/images/logo.jpg"); // Get the logo URL from the resources
+        ImageIcon logoIcon = new ImageIcon(logoUrl); // Replace with your logo path
         logoLabel.setIcon(logoIcon); // Add the logo to the label
         topPanel.add(logoLabel, BorderLayout.CENTER); // Add the label to the top panel
 
